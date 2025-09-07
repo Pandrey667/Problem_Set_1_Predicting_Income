05d658e166fae098463f64f4760d6e48d6d6de12
Fabián Ernesto Vidal Sánchez <fevidals10@gmail.com>
2025-09-07 17:19:58 -0500

    Tercer update

--- scripts/Punto_2_a.R
+++ scripts/Punto_2_a.R
@@ -47,3 +47,48 @@ class(data_final)
 
 ### Estudiamos las columnas del data frame 
 glimpse(data_final)
+
+### Filtramos, con la librería Tidyverse, data_final
+### Personas estrictamente mayores de 18 años
+### Personas ocupadas (ocu =1)
+
+df_1 <- data_final %>% 
+  filter(age >= 18 & ocu %in% 1)
+
+
+### Obtención de la variable salario por hora para cada individuo
+df_2 <- df_1 %>% 
+  ## Filtramos para obtener solo individuos tienen horas trabajadas
+  filter(totalHoursWorked != 0) %>% 
+  mutate(
+    ## Creamos la variable ingreso_total que está compuesta por la sumatoria
+    ## de impaes + isaes. Es decir: ingresos del trabajo principal + ingresos de un posible trabajo adicional
+    ingreso_total = ifelse(!is.na(impa), impa, impaes) +
+      ifelse(!is.na(isa),  isa,  isaes),
+    ## Obtenemos el total de horas trabajadas al mes (multiplicamos totalHoursWorked x 4)
+    horas_mes_Worked = totalHoursWorked * 4,
+    ## El salario por hora (salario_hora) es la división del ingreso total sobre las horas trabajas al mes (obtenidas con anterioridad) 
+    salario_hora = ingreso_total / horas_mes_Worked
+  )
+
+#################### Gráficos de las variables empleadas en las regresiones #############################################
+
+
+### Filtramos los NA y valores iguales a cero en 
+df_2 <- df_2 %>% 
+  filter(!is.na(salario_hora)) %>% 
+  filter(salario_hora != 0) %>% 
+  filter(!is.na(maxEducLevel))
+
+### Creamos la variable del logaritmo natural del salario por hora
+df_2$ln_wage <- log(df_2$salario_hora)
+
+### Convertimos df_2 a un objeto data frame
+df_2 <- as.data.frame(df_2)
+
+
+### Construímos el data frame variables_num con las variables numéricas
+variables_num <- df_2 %>% 
+  select(salario_hora, 
+         ln_wage, age, 
+         totalHoursWorked)
