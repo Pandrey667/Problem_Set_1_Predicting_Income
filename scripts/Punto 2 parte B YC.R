6263774be3a254b3959ce83aaf20f39c745f23d1
Yenny Castillo <y.castillor@uniandes.edu.co>
2025-09-07 18:46:14 -0500

    Histogramas salario por hora
    Se incluyen histogramas del salario por hora en logaritmo y niveles

--- scripts/Punto 2 parte B YC.R
+++ scripts/Punto 2 parte B YC.R
@@ -42,3 +42,44 @@ datasummary_df(cat_summary,
                output = "latex",
                title = "Estadísticas descriptivas de variables categóricas")
 
+
+########################## Gráficas de las variables ########################################################################################################
+
+#### Histograma del salario por hora
+test_1 <- df_2 %>% 
+  ggplot(., aes(x = salario_hora)) + 
+  geom_histogram(bins = 15, 
+                 fill = "darkgoldenrod1", 
+                 color = "black") + 
+  ylab('Frecuencia') + 
+  xlab('Salario por hora (Pesos colombianos)') + 
+  ggtitle('Salarios por hora') + 
+  theme(plot.title = element_text(hjust = 0.5, 
+                                  size = 17), 
+        plot.caption = element_text(hjust = 0))
+#labs(caption = "Fuente: Gran Encuesta Integrada de Hogares (GEIH) 2018. Gráfica de elaboración propia.")
+
+
+#### Histograma del logaritmo natural del salario por hora
+test_2 <- df_2 %>%
+  ggplot(., aes(x = ln_wage)) + 
+  geom_histogram(bins = 15, 
+                 fill = "darkgoldenrod1", 
+                 color = "black") + 
+  ylab('Frecuencia') + 
+  xlab('Escala logarítmica') + 
+  ggtitle('Logaritmo natural del Salario por hora') + 
+  theme(plot.title = element_text(hjust = 0.5, 
+                                  size = 17), 
+        plot.caption = element_text(hjust = 0))
+#labs(caption = "Fuente: Gran Encuesta Integrada de Hogares (GEIH) 2018")
+
+### Matriz de gráficos del salario por hora
+salarios_graph <- grid.arrange(test_1, test_2, ncol = 2)
+
+### Exportamos la matriz de gráficos del salario
+ggsave('salarios.jpeg', plot = salarios_graph, 
+       width = 10, height = 6, dpi = 300)
+
+
+
